c293112ce0aa137582fbdbcd91e994f8aca1c05f
cal1pa <141686747+cal1pa@users.noreply.github.com>
2025-07-20 21:12:11 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,25 +6,36 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	STATUS_LINE_OK = "HTTP/1.1 200 OK"
+	CRLF           = "\r\n"
+)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	res := fmt.Sprintf("%s%s%s", STATUS_LINE_OK, CRLF, CRLF)
+
+	_, err = conn.Write([]byte(res))
+	if err != nil {
+		fmt.Println("Failed to write response: ", err.Error())
+		os.Exit(1)
+	}
+	conn.Close()
 }
